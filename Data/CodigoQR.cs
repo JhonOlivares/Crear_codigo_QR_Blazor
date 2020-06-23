63d08d22a840c9924eab38edca661043fef247f0
John local <jnmatt@icloud.com>
2020-06-22 22:34:28 -0500

    first

---
+++ Data/CodigoQR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+namespace CodigoQRBlazor.Data
+{
+    public class CodigoQR
+    {
+        public string Texto { get; set; }
+        public string CodigoQRstr { get; set; }
+
+        public void QRGenerator()
+        {
+            CodigoQRstr = "";
+            if (!string.IsNullOrEmpty(Texto))
+            {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    QRCodeGenerator codeGenerator = new QRCodeGenerator();
+                    QRCodeData coreData = codeGenerator.CreateQrCode(Texto, QRCodeGenerator.ECCLevel.Q);
+                    QRCode codigoQR = new QRCode(coreData);
+                    using (Bitmap bitmap = codigoQR.GetGraphic(20))
+                    {
+                        bitmap.Save(ms, ImageFormat.Png);
+                        CodigoQRstr = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Generador de código QR en Blazor
+ASP.NET Core 
+## Info 
+
+Una forma simple de genarar códigos QR en un proyecto de Blazor usando la librería QRCoder.
+
+Para mas información visita:
+[**QRCode Wiki**](https://github.com/codebude/QRCoder/wiki) | [blog del creador (inglés)](http://en.code-bude.net/2013/10/17/qrcoder-an-open-source-qr-code-generator-implementation-in-csharp/)
